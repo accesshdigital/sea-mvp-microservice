2277b18f28e96c3631613f07428c96172b4f27c2
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2020-05-29 20:10:29 +0530

    infrastructure microservices

---
+++ invokertech-admin-server/src/main/java/com/invokertech/admin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+
+package com.invokertech.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
