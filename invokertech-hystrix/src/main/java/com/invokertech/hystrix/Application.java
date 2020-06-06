7c837d07064e47b911eb3acb087a636954aac16a
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2020-06-06 19:44:52 +0530

    infrastructure microservices

--- invokertech-hystrix/src/main/java/com/invokertech/hystrix/Application.java
+++ invokertech-hystrix/src/main/java/com/invokertech/hystrix/Application.java
@@ -1,5 +1,4 @@
 package com.invokertech.hystrix;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
