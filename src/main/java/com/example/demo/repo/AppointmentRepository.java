afd3f58858c6fe87f6092286b180479bdae2666f
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2021-01-25 09:44:01 +0530

    Delete src directory

--- src/main/java/com/example/demo/repo/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Appointment;
-
-	public interface AppointmentRepository extends JpaRepository<Appointment, Integer>{  
-
-}
--- src/main/java/com/example/demo/ved/Ag.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.ved;
-
-public class Ag {
-
-}
