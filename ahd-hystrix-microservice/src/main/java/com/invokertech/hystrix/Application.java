155f9705c721d69f3fb2ec602c4a314fa675fd2a
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2021-01-28 14:22:39 +0530

    Add files via upload

---
+++ ahd-hystrix-microservice/src/main/java/com/invokertech/hystrix/Application.java
@@ -0,0 +1,14 @@
+package com.invokertech.hystrix;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+
+@SpringBootApplication		
+@EnableCircuitBreaker		
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ ahd-hystrix-microservice/src/main/java/com/invokertech/hystrix/model/Test.java
@@ -0,0 +1,40 @@
+package com.invokertech.hystrix.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+	private int id;
+	private String test1;
+	private String test2;
+	private String test3;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTest1() {
+		return test1;
+	}
+	public void setTest1(String test1) {
+		this.test1 = test1;
+	}
+	public String getTest2() {
+		return test2;
+	}
+	public void setTest2(String test2) {
+		this.test2 = test2;
+	}
+	public String getTest3() {
+		return test3;
+	}
+	public void setTest3(String test3) {
+		this.test3 = test3;
+	}
+	
+	
+}
+	
+	
