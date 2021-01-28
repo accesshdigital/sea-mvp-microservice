6725ff9eee8b8dba5d0c357caea3c4291e4c0d91
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2021-01-28 14:05:27 +0530

    Add files via upload

---
+++ ahd-jwt-auth-micrpservice/src/main/java/com/invokertech/jwt/dto/UserDataDTO.java
@@ -0,0 +1,52 @@
+package com.invokertech.jwt.dto;
+
+import java.util.List;
+
+import com.invokertech.jwt.model.Role;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class UserDataDTO {
+  
+  @ApiModelProperty(position = 0)
+  private String username;
+  @ApiModelProperty(position = 1)
+  private String email;
+  @ApiModelProperty(position = 2)
+  private String password;
+  @ApiModelProperty(position = 3)
+  List<Role> roles;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public List<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<Role> roles) {
+    this.roles = roles;
+  }
+
+}
